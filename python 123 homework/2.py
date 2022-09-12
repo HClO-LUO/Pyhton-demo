5e1d949d43bae06a22697bcef2b3adc0ffc9e48e
HClO-LUO <276306519@qq.com>
2022-09-12 18:04:00 +0800

    Update 2.py

--- python 123 homework/2.py
+++ python 123 homework/2.py
@@ -4,4 +4,5 @@
 #https://github.com/HClO-LUO/Pyhton-demo.git
 name=str(input())
 word=str(input())
-print('{}，我想对你说，{}'.format(name,word),sep=',')
+print('{}，我想对你说，{}'.format(name,word))
+print(name,'我想对你说',word,sep='，')
