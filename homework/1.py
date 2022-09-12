cf9a0ee8de8af0094a6546e69bfd4045a4fe8887
HClO-LUO <276306519@qq.com>
2022-09-12 16:18:09 +0800

    Create 1.py

---
+++ homework/1.py
@@ -0,0 +1,12 @@
+# HClO Luo
+# 小罗
+# 2022/9/12 16:07
+#
+list1=[32,43,12,23,1,3,5]
+aver=sum(list1)/len(list1)
+s=0
+for i in range(0,len(list1)):
+    s+=list1[i]
+print('sum=',s)
+print('aver=',aver)
+
